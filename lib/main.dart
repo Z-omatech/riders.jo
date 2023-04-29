a4d650cb88e7a3ef8e8e6d5e38ad9a367ce33c82
ziadalsafe <zalsafe4@gmail.com>
2023-04-29 14:50:35 +0300

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:riders_jo/screen/welcome.dart';
-=======
-import 'package:get/get_navigation/get_navigation.dart';
-
 import 'featuers/splash/views/splash_view.dart';
->>>>>>> origin/ziad
 
 void main() {
   runApp(const RidersJO());
 }
 
-<<<<<<< HEAD
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Welcome(),
-      debugShowCheckedModeBanner: false,
-=======
 class RidersJO extends StatelessWidget {
   const RidersJO({super.key});
 
@@ -35,7 +14,6 @@ class RidersJO extends StatelessWidget {
       theme: ThemeData(fontFamily: 'Poppins'),
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
->>>>>>> origin/ziad
     );
   }
 }
